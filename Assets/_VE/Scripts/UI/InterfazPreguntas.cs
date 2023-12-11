5a616c3404b039ef0f314eef40b881aba31657ed
Firesnoww <Miguelpela222@gmail.com>
2023-12-11 15:53:00 -0500

    preguntas2.0

--- Assets/_VE/Scripts/UI/InterfazPreguntas.cs
+++ Assets/_VE/Scripts/UI/InterfazPreguntas.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class InterfazPreguntas : MonoBehaviour
 {
     public GameObject[] entrada;
-
+    public GameObject[] salida;
+    bool Paso = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,22 +17,32 @@ public class InterfazPreguntas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void ActivarEntrada()
-    {
-        for (int i = 0; i < entrada.Length; i++)
+        if (Input.GetKey("2"))
         {
-            entrada[i].gameObject.SetActive(true);
+            Accion();
         }
-        
     }
-    public void ActivarSalida()
-    {
-        for (int i = 0; i < entrada.Length; i++)
-        {
-            entrada[i].gameObject.SetActive(false);
-        }
 
+    public void Accion() 
+    {
+        StartCoroutine("TiempoCd");
+    }
+    public IEnumerator TiempoCd() 
+    {
+
+        for (int i = 0; i < salida.Length; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            salida[i].gameObject.SetActive(Paso);
+        }
+        yield return new WaitForSeconds(0.5f);
+        Paso = true;
+        for (int i = 0; i < entrada.Length; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            entrada[i].gameObject.SetActive(Paso);
+        }
+        yield return new WaitForSeconds(0.5f);
+        Paso = false;
     }
 }
